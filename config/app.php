9737ecaf5097dbaa0158162640761bfa3c63edef
Damar Permadany <damarp2017@gmail.com>
2021-06-11 07:02:01 +0700

    remove barryvdh/laravel-ide-helper

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         // Additional Provider
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        // Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
