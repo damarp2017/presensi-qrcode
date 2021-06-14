f239dbe917e4cf7027e17fc6545193345a852643
Damar Permadany <damarp2017@gmail.com>
2021-06-14 12:07:01 +0700

    test

--- resources/views/admin/attendance/attendance.blade.php
+++ resources/views/admin/attendance/attendance.blade.php
@@ -2,64 +2,64 @@
 @push('styles')
 <link rel="stylesheet" href="{{ asset('assets/adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
 <style media="screen">
-video {
-/* override other styles to make responsive */
-    width: 100%    !important;
-    height: auto   !important;
+  video {
+    /* override other styles to make responsive */
+    width: 100% !important;
+    height: auto !important;
   }
 </style>
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 @endpush
 @section('main')
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Absensi Siswa</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item">Absensi Siswa</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Absensi Siswa</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item">Absensi Siswa</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
 
-    <!-- Main content -->
-    <div class="content">
-        <div class="container-fluid">
-          <div class="row">
-              <div class="col-md-8">
-                  <div class="card">
-                    <div class="card-body">
-                      <div class="row">
-                        <div class="col-md-6">
-                          <h5 class="card-title mb-3">Kamera Scan Absensi</h5>
-                        </div>
-                        <div class="col-md-6">
-                            <h5 class="card-title mb-3 float-right" id="clock-wrapper"></h5>
-                        </div>
-                      </div>
-                      <video id="preview"></video>
-                    </div>
-                  </div>
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-6">
+                  <h5 class="card-title mb-3">Kamera Scan Absensi</h5>
                 </div>
-                <div class="col-md-4">
-                  <div class="card">
-                    <div class="card-body">
-                      <h5 class="card-title mb-3">Absensi Siswa</h5>
-                    </div>
-                  </div>
+                <div class="col-md-6">
+                  <h5 class="card-title mb-3 float-right" id="clock-wrapper"></h5>
                 </div>
+              </div>
+              <video id="preview"></video>
             </div>
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-3">Test Absensi Siswa</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
 </div>
 @endsection
 
@@ -67,7 +67,7 @@ video {
 <script src="{{ asset('assets/adminlte/plugins/instascan/instascan.min.js') }}"></script>
 <script src="{{ asset('assets/adminlte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
 <script type="text/javascript">
-    const soundSuccess = '/assets/adminlte/sound/success.mp3';
+  const soundSuccess = '/assets/adminlte/sound/success.mp3';
     const soundFailed = '/assets/adminlte/sound/failed.mp3';
     const start = new Date;
 
@@ -131,5 +131,5 @@ video {
          })
        }
 
-   </script>
+</script>
 @endpush
